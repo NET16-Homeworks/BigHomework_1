7fccd7c73e524b9ca415ea69e91fac97c6df1576
VladPulyak <vlad.pulyak03@mail.ru>
2022-10-06 16:49:29 +0300

    Initial commit

---
+++ BigHomework_1/BigHomework_1/LoanCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigHomework_1
+{
+    internal static class LoanCalculator
+    {
+        public static void Calculate(double sumOfLoan, int monthCount, double percent)
+        {
+            double percentRate = percent / 1200;
+            double remainderOfCurrentPeriod;
+            double amountOfCredit = sumOfLoan * (percentRate + (percentRate / (Math.Pow(1 + percentRate, monthCount) - 1)));
+            double mainDept;
+            for (int i = 1; i < monthCount + 1; i++)
+            {
+                Console.WriteLine($"Amount of payment for {i} month: {amountOfCredit}");
+                mainDept = amountOfCredit - sumOfLoan * percentRate;
+                remainderOfCurrentPeriod = sumOfLoan - mainDept;
+                Console.WriteLine($"Remainder: {remainderOfCurrentPeriod}");
+                Console.WriteLine("=========");
+                sumOfLoan = remainderOfCurrentPeriod;
+            }
+        }
+    }
+}
--- BigHomework_1/BigHomework_1/Program.cs
+++ BigHomework_1/BigHomework_1/Program.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BigHomework_1;
+int enteredNumber = 0;
+while (enteredNumber != -1)
+{
+    Menu();
+    Console.WriteLine("Enter number of operation");
+    enteredNumber = int.Parse(Console.ReadLine());
+    switch (enteredNumber)
+    {
+        case 1:
+            {
+                UserService.CreateUser();
+                break;
+            }
+        case 2:
+            {
+                Console.WriteLine("Enter email of deleted user");
+                string email = Console.ReadLine();
+                UserService.DeleteUser(email);
+                break;
+            }
+        case 3:
+            {
+                Console.WriteLine("Enter email");
+                string email = Console.ReadLine();
+                UserService.WriteUserInfo(email);
+                break;
+            }
+        case 4:
+            {
+                LoanManager.SuggestLoan();
+                break;
+            }
+        case 5:
+            {
+                Console.WriteLine("Enter email:");
+                string email = Console.ReadLine();
+                LoanManager.PrintInformationAboutUserLoans(email);
+                break;
+            }
+        case -1:
+            {
+                break;
+            }
+    }
+}
+void Menu()
+{
+    Console.WriteLine(@"1. Add User" + Environment.NewLine +
+                       "2. Delete User" + Environment.NewLine +
+                       "3. Print Information about User" + Environment.NewLine +
+                       "4. Suggest credit" + Environment.NewLine +
+                       "5. Print Information About user credits" + Environment.NewLine + 
+                       "-1. Enter to exit program");
+}
