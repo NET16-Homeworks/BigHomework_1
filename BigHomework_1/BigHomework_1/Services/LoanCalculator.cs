4d108e68767680a75cd81491fdd7d78d93f2e9de
Ruslan Soroko <rus.soroko@gmail.com>
2022-10-21 02:47:37 +0300

    variable rename

--- BigHomework_1/BigHomework_1/Services/LoanCalculator.cs
+++ BigHomework_1/BigHomework_1/Services/LoanCalculator.cs
@@ -10,14 +10,14 @@ namespace BigHomework_1.Services
     {
         public static void Calculate(double loanAmount, int loanTerm, double loanRate)
         {
-            double monthInterestRate = loanRate / 1200;
-            double monthPayment = loanAmount * (monthInterestRate + (monthInterestRate / (Math.Pow(1 + monthInterestRate, (double)loanTerm) - 1)));
-            double balance = loanAmount - monthPayment + loanAmount * monthInterestRate;
+            double monthLoanRate = loanRate / 1200;
+            double monthPayment = loanAmount * (monthLoanRate + (monthLoanRate / (Math.Pow(1 + monthLoanRate, (double)loanTerm) - 1)));
+            double balance = loanAmount - monthPayment + loanAmount * monthLoanRate;
             Console.WriteLine("Остаток по кредиту   Ежемесячный платеж");
             for (int i = 0; i < loanTerm; i++)
             {
                 Console.WriteLine("{0:f2}\t\t\t{1:f2}", balance, monthPayment);
-                balance = balance - monthPayment + balance * monthInterestRate;
+                balance = balance - monthPayment + balance * monthLoanRate;
             }
 
         }
