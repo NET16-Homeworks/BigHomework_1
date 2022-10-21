2c24a75b92ab59399ceac3aed0a2045175f70c4a
Ruslan Soroko <rus.soroko@gmail.com>
2022-10-21 19:16:50 +0300

    Добавил обработку исключений в меню

--- BigHomework_1/BigHomework_1/Program.cs
+++ BigHomework_1/BigHomework_1/Program.cs
@@ -11,38 +11,45 @@ bool isContinued = true;
 while (isContinued)
 {
     Console.WriteLine("Выберите операцию:\n1.Добавить пользователя\n2.Удалить пользователя\n3.Вывести информацию о пользователе\n4.Предложить кредит");
-    bool flag = int.TryParse(Console.ReadLine(), out var choice);
-    if (!flag)
+    try
     {
-        throw new InvalidInputException();
+        bool flag = int.TryParse(Console.ReadLine(), out var choice);
+        if (!flag)
+        {
+            throw new InvalidInputException();
+        }
+
+        switch (choice)
+        {
+            case 1:
+                userService.CreateUser();
+                break;
+            case 2:
+                userService.DeleteUser();
+                break;
+            case 3:
+                userService.WriteUserInfo();
+                break;
+            case 4:
+                loanManager.SuggestedLoan();
+                break;
+            default:
+                Console.WriteLine("Данная операция не существует!");
+                break;
+        }
+
+        Console.WriteLine("Желаете продолжить работу? (да/нет)");
+        string input = InputCheckMethods.StringReadAndCheck();
+        input.Trim()
+             .ToLower();
+
+        if (input == "нет")
+        {
+            isContinued = false;
+        }
     }
-
-    switch (choice)
+    catch (Exception ex)
     {
-        case 1:
-            userService.CreateUser();
-            break;
-        case 2:
-            userService.DeleteUser();
-            break;
-        case 3:
-            userService.WriteUserInfo();
-            break;
-        case 4:
-            loanManager.SuggestedLoan();
-            break;
-        default:
-            Console.WriteLine("Данная операция не существует!");
-            break;
-    }
-
-    Console.WriteLine("Желаете продолжить работу? (да/нет)");
-    string input = InputCheckMethods.StringReadAndCheck();
-    input.Trim()
-         .ToLower();
-
-    if (input == "нет")
-    {
-        isContinued = false;
+        Console.WriteLine(ex.Message);
     }
 }
