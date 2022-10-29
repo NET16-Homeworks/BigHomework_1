35b244fe4ace8e25443001a64f4062f6d7ec16f3
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-10-29 23:09:05 +0300

    Create User class

---
+++ BigHomework_1/BigHomework_1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigHomework_1
+{
+    public sealed class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"FullName: {FirstName} {LastName}, Email: {Email}, Age: {Age}";
+        }
+    }
+}
